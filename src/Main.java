1932d2af0d9b3a1b19377d969e555500ac324296
Elber Gutierrez <gutiere5@ferris.edu>
2024-05-29 22:33:12 -0400

    Added code to allow the program allow multiple coin tosses

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scnr = new Scanner(System.in);
+		Random random = new Random(); 
+		int randomNumber;
+		boolean run;
+		String userInput;
 
+		
+
+		System.out.println("Welcome to Coin Toss Simulator");
+
+		run = true; 
+		while (run) {
+			
+			randomNumber = random.nextInt(2);
+			
+			if (randomNumber == 1) {
+				System.out.println("You got tails");
+			}
+			else {
+				System.out.println("You got heads");
+
+			}
+			
+			System.out.println("Another Toss? y or n");
+			userInput = scnr.nextLine();
+			
+			if (userInput.equals("n")) {
+				run = false;
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
