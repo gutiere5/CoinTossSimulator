d5fc261b60a1d2c25e4e39bdc0b50b6cb68e5deb
Elber Gutierrez <gutiere5@ferris.edu>
2024-05-29 22:38:22 -0400

    Added the ability to track the sum of heads and tails during a session

--- src/Main.java
+++ src/Main.java
@@ -6,45 +6,50 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		Random random = new Random(); 
-		int randomNumber;
-		boolean run;
 		String userInput;
-
-		
+		int randomNumber;
+		int headsCount = 0;
+		int tailsCount = 0;
+		boolean run;
 
 		System.out.println("Welcome to Coin Toss Simulator");
 
 		run = true; 
 		while (run) {
-			
 			randomNumber = random.nextInt(2);
-			
+
 			if (randomNumber == 1) {
 				System.out.println("You got tails");
+				tailsCount++;
 			}
 			else {
 				System.out.println("You got heads");
-
+				headsCount++;
 			}
-			
+
 			System.out.println("Another Toss? y or n");
 			userInput = scnr.nextLine();
-			
+
 			if (userInput.equals("n")) {
 				run = false;
 			}
-			
-			
+
+
+
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+		// Displays Statitics
+		System.out.println("Statistics:");
+		System.out.println("Heads: " + headsCount);
+		System.out.println("Tails: " + tailsCount);
+
+
+
 	}
 
 }
